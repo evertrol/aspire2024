53ba2810431b602aff1ecc3298137500cd76f760
Evert Rol <@>
2024-07-03 12:50:08 +0200

    Split the elliptical orbit function
    This creates a separate elliptical initialisation function,
    simplifying the `run_rk4_elliptical()` function.

--- src/aspire/core.py
+++ src/aspire/core.py
@@ -6,7 +6,7 @@ from .integrators.states import OrbitState
 from .constants import GM
 
 
-__all__ = ["GM", "init_circular"]
+__all__ = ["GM", "init_circular", "init_elliptical"]
 
 
 def init_circular():
@@ -15,6 +15,20 @@ def init_circular():
     x0 = 0
     y0 = 1
     u0 = -np.sqrt(GM / y0)
+    v0 = 0
+
+    return OrbitState(x0, y0, u0, v0)
+
+
+def init_elliptical():
+    """Set initial conditions for an elliptical orbit"""
+
+    a = 1.0
+    e = 0.6
+
+    x0 = 0
+    y0 = a * (1 - e)
+    u0 = -np.sqrt(GM / a * (1 + e) / (1 - e))
     v0 = 0
 
     return OrbitState(x0, y0, u0, v0)
--- src/aspire/run.py
+++ src/aspire/run.py
@@ -1,11 +1,8 @@
 """Module with helper runner functions"""
 
-import numpy as np
-
 from .plotting.core import plot
-from .core import init_circular, GM
+from .core import init_circular, init_elliptical
 from .integrators import integrate_euler, integrate_rk2, integrate_rk4
-from .integrators.states import OrbitState
 
 
 __all__ = ["run_euler", "run_rk2", "run_rk4", "run_rk4_elliptical"]
@@ -70,23 +67,21 @@ def run_rk4(taus):
     return fig
 
 
-def run_rk4_elliptical():
-    """Run a fourth-order Runge-Kutta orbit integration, for a circular orbit"""
+def run_rk4_elliptical(taus):
+    """Run a fourth-order Runge-Kutta orbit integration, for an elliptical orbit"""
 
-    a = 1.0
-    e = 0.6
+    state0 = init_elliptical()
 
-    x0 = 0
-    y0 = a * (1 - e)
-    u0 = -np.sqrt(GM / a * (1 + e) / (1 - e))
-    v0 = 0
+    fig = None
+    for tau in taus:
+        _, history = integrate_rk4(state0, tau, 1)
 
-    state0 = OrbitState(x0, y0, u0, v0)
+        label = rf"$\tau = {tau:6.4f}$"
+        if not fig:
+            fig = plot(history, label=label)
+        else:
+            plot(history, ax=fig.gca(), label=label)
 
-    tau = 0.025
-
-    _, history = integrate_rk4(state0, tau, 1)
-
-    fig = plot(history)
+    fig.gca().legend()
 
     return fig
